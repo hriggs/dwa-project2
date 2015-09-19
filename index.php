4be7749b25d7a4c5046c5ec1abc6eb352d9dd5dc
Hannah Riggs <hkriggs128@gmail.com>
2015-09-18 21:51:48 -0400

    Desktop and responsive styles added.

--- index.php
+++ index.php
@@ -26,34 +26,56 @@ CSCI E-15: Dynamic Web Applications
 			<div class="col-lg-8 col-md-8 col-sm-8 col-xs-8 content">
 				<h1><span class="xkcd">xkcd</span> Password Generator</h1>
 				<p class="description">Description: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+				<p class="directions">Customize your password with the form below or use the default values.<br><span class="bold">Nine is the maximum number</span> that can be entered in a text field.</p>
 				<form method="POST" action="index.php">
 					<label for='word_num'>Number of Words:</label>
-					<input maxlength=1 type="text" name="word_num" value="4">  (Max 9)
+					<div class="br">
+						<br>
+					</div>
+					<input maxlength=1 type="text" name="word_num" value="4" class="form-box">
 					<br>
 					<label for="number_num">Number of Numbers:</label>
-					<input maxlength=1 type="text" name="number_num"> (Max 9)
+					<div class="br">
+						<br>
+					</div>
+					<input maxlength=1 type="text" name="number_num" value="1" class="form-box">
 					<br>
 					<label for="number_loc">Number Location:</label>
+					<div class="br">
+						<br>
+					</div>
 					<input type="radio" name="number_loc" value="end" checked>At End 
 					<input type="radio" name="number_loc" value="random">Random
 					<br>
 					<label for="symbol_num">Number of Symbols:</label>
-					<input maxlength=1 type="text" name="symbol_num">  (Max 9)
+					<div class="br">
+						<br>
+					</div>
+					<input maxlength=1 type="text" name="symbol_num" value="1" class="form-box">
 					<br>
 					<label for="symbol_loc">Symbol Location:</label>
+					<div class="br">
+						<br>
+					</div>
 					<input type="radio" name="symbol_loc" value="end" checked>At End 
 					<input type="radio" name="symbol_loc" value="random">Random
 					<br>
 					<label for="separator">Separate Words with:</label>
-					<select name="separator">
+					<div class="br">
+						<br>
+					</div>
+					<select name="separator" class="form-box">
 						<option value="hyphens">Hyphens</option>
   						<option value="spaces">Spaces</option>
   						<option value="nospace">No Characters</option>
 					</select> 
 					<br>
 					<label for="cases">Word Cases:</label>
-					<select name="cases">
-						<option value="start">First Letter of Each Word Capitalized </option>
+					<div class="br">
+						<br>
+					</div>
+					<select name="cases" class="form-box">
+						<option value="start">First Letter Capitalized</option>
   						<option value="upper">All Upper Case</option>
   						<option value="lower">All Lower Case</option>
 					</select> 
@@ -61,7 +83,7 @@ CSCI E-15: Dynamic Web Applications
 					<input type="submit" class="btn btn-primary" value="Generate Password">
 				</form>
 				<div class="password">
-					<p><?php echo get_password($word_list, $symbols_list); print_r($_POST); ?></p>
+					<p><?php echo get_password($word_list, $symbols_list); /*print_r($_POST);*/ ?></p>
 				</div>
 				<div class="row">
 					<div class="col-lg-2 col-md-2 col-sm-0 col-xs-0"></div>
